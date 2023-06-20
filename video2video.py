61145c9019ebdf4829f5bd1d9042052765faa9ff
Артём <77682641+Artalmaz31@users.noreply.github.com>
2023-06-20 21:04:39 +0300

    Update video2video.py

--- video2video.py
+++ video2video.py
@@ -5,7 +5,7 @@ from PIL import Image
 import numpy
 
 device = "cuda"
-model_path = "CompVis/stable-diffusion-v1-4"
+model_path = "runwayml/stable-diffusion-v1-5"
 
 pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
     model_path,
@@ -18,23 +18,23 @@ pipe = pipe.to(device)
 
 video_file = "video.mp4"
 capture = cv2.VideoCapture(video_file)
-prompt = "cinematic style, strong colors, a masterpiece"
+prompt = "anime style, highly detailed, high quality, beautiful masterpiece"
 
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 video = cv2.VideoWriter(
-    filename="output.mp4", fourcc=fourcc, fps=30.0, frameSize=(1920, 1080)
+    filename="output.mp4", fourcc=fourcc, fps=30.0, frameSize=(720, 405)
 )
 
 while True:
     success, frame = capture.read()
- 
+    
     if success:
         generator = torch.Generator(device=device).manual_seed(1024)
         init_image = Image.fromarray(frame).convert("RGB")
-        init_image = init_image.resize((1920, 1080))
-        image = pipe(prompt=prompt, image=init_image, strength=0.75, guidance_scale=7.5, generator=generator).images[0]
+        init_image = init_image.resize((720, 405))
+        image = pipe(prompt=prompt, image=init_image, strength=0.25, guidance_scale=7.5, generator=generator).images[0]
         image = numpy.asarray(image)
-        image = cv2.resize(image, dsize=(1920, 1080))
+        image = cv2.resize(image, dsize=(720, 405))
         video.write(image)
  
     else:
